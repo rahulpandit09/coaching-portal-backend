cacba62f729492677282bbe880204c098326c714
Rahul kumar <rahul.kashyaprp09@gmail.com>
2026-06-19 20:34:31 +0530

    changed auth from oauth2 to bearer

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
@@ -7,14 +7,14 @@ from app.database import get_db
 from app.models.user import User
 from app.core.config import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+security = HTTPBearer()
 
 
-# ================= AUTHENTICATION =================
 def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(security),
     db: Session = Depends(get_db)
-) -> User:
+):
+    token = credentials.credentials
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -43,7 +43,6 @@ def get_current_user(
     return user
 
 
-# ================= AUTHORIZATION =================
 def admin_required(user: User = Depends(get_current_user)):
     if user.role != "admin":
         raise HTTPException(status_code=403, detail="Admin only")
