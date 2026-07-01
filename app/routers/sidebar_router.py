806806deaeabbc02c32c168211327b108cf845f3
rahulpandit09 <rahul.kashyaprp09@gmail.com>
2026-07-01 18:03:57 +0530

    fixed empty sidebar response

--- app/routers/sidebar_router.py
+++ app/routers/sidebar_router.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.core.security import get_current_user
 from app.services.sidebar_service import sidebar_service
-from app.schemas.sidebar_schema import SidebarResponse
-
+from app.schemas.sidebar_schema import SidebarResponse, SidebarListResponse
 
 
 router = APIRouter(
@@ -13,15 +12,17 @@ router = APIRouter(
     tags=["sidebar"]
 )
 
-@router.get("/",response_model=SidebarListResponse)
+
+@router.get("/", response_model=SidebarListResponse)
 def get_sidebar(
     db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+    current_user=Depends(get_current_user)
 ):
     menus = sidebar_service(
         db,
         current_user.role_id
     )
+
     return {
         "count": len(menus),
         "data": menus
